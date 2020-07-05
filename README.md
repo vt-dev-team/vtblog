626633b453466087c727c439b87c221c33846962
yemaster <yemaster@ahstu.cc>
2020-07-05 19:44:42 +0800

    Update 202007051944

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # vtblog
 
 A simple blog system with FaseApi
+
+## Installation
+
+### 1. Install `FastApi`
+
+```
+pip install fastapi
+pip install uvicorn
+```
+
+### 2. Clone this project
+
+```
+git clone https://github.com/vt-dev-team/vtblog
+```
+
+### 3. Start Backend
+
+```
+uvicorn vtblog.main:app --reload
+```
--- crud.py
+++ crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+import time
 from . import models, schemas
 
 def get_post(db: Session, post_id: int):
@@ -8,4 +9,4 @@ def get_posts(db: Session, skip: int = 0, limit: int = 20):
     return db.query(models.Post).offset(skip).limit(limit).all()
 
 def create_user(db: Session, post: schemas.PostCreate):
-    db_post = models.Post(title=post.title, content=post.content, tags=post.tags, toplevel=post.toplevel, date=post.date, defunct=post.defunct)
+    db_post = models.Post(title=post.title, content=post.content, tags=post.tags, toplevel=post.toplevel, date=(int)(time.time()), defunct=post.defunct)
